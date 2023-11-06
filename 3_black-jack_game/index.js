66376962a472df6a63fb9320d4c4ce270b7ad2cd
mohammad-aly <riyazmohammadrj786@gmail.com>
2023-11-06 10:01:57 +0530

    todays practice

--- 3_black-jack_game/index.js
+++ 3_black-jack_game/index.js
@@ -1,3 +1,7 @@
+let player = {
+    name : "Riyaz",
+    chips : 124
+}
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,8 +11,11 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+// console.log(cards);
 
-console.log(cards);
+playerEl.textContent = player.name + ": $" + player.chips
+
 
 function getRandomCard(){
     let random = Math.floor(Math.random()*13)+1;
@@ -60,8 +67,10 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card);
-    renderGame();
+    if(hasBlackJack === false && isAlive === true){
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
 }
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 let myPoints = 3;
 
 function add3points(){
@@ -18,3 +19,37 @@ remove1point();
 
 
 console.log(myPoints);
+*/
+
+//objects and functions
+
+// let person = {
+//     name: 'John',
+//     age: 22,
+//     country: "India"
+// }
+
+// function logData(){
+//     let data = person.name + " is " + person.age + " years old and lives in "+ person.country;
+//     console.log(data);
+// }
+
+// logData();
+
+//loops and arrays
+
+// let largeCountries = ["china", "india", "USA", "Indonesia", "Pakistan"];
+
+// console.log("The 5 largest countries in the world: ")
+// for(let i=0; i<largeCountries.length; i++){
+//     console.log("-" + largeCountries[i]);
+// }
+
+let largeCountries = ["Tuvalu", "india", "USA", "Indonesia", "Monaco"];
+
+largeCountries.pop();
+largeCountries.push("Pakistan");
+
+largeCountries.shift(); //deletes the first starting item
+largeCountries.unshift("China") //adds the new item from satrating
+console.log(largeCountries);
